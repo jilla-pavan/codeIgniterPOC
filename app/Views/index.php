d90cf744c7bd8646eb3462ff255b4c2bf34f3af3
jilla-pavan <jillapavan22001@gmail.com>
2024-12-18 18:40:25 +0530

    codeignitor POC

---
+++ app/Views/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>Index - iLanding Bootstrap Template</title>
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+
+  <!-- Favicons -->
+  <link href="<?php echo base_url('public/assets/img/favicon.png'); ?>" rel="icon">
+  <link href="<?php echo base_url('public/assets/img/apple-touch-icon.png'); ?>" rel="apple-touch-icon">
+
+  <!-- Fonts -->
+  <link href="<?php echo base_url('https://fonts.googleapis.com'); ?>" rel="preconnect">
+  <link href="<?php echo base_url('https://fonts.gstatic.com'); ?>" rel="preconnect" crossorigin>
+  <link href="<?php echo base_url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'); ?>" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="<?php echo base_url('public/assets/vendor/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
+  <link href="<?php echo base_url('public/assets/vendor/bootstrap-icons/bootstrap-icons.css'); ?>" rel="stylesheet">
+  <link href="<?php echo base_url('public/assets/vendor/aos/aos.css'); ?>" rel="stylesheet">
+  <link href="<?php echo base_url('public/assets/vendor/glightbox/css/glightbox.min.css'); ?>" rel="stylesheet">
+  <link href="<?php echo base_url('public/assets/vendor/swiper/swiper-bundle.min.css'); ?>" rel="stylesheet">
+
+  <!-- Main CSS File -->
+  <link href="assets/css/main.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<?= $this->include('header') ?>
+
+
+<?= $this->include('content') ?>
+
+
+<?= $this->include('footer') ?>
+
+  <!-- Scroll Top -->
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+
+  <!-- Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
